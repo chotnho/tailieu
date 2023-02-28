29df44f3c88e99e3299734e7a3304a393cbef64b
thiendi0 <87894638+thiendi0@users.noreply.github.com>
2023-02-28 13:44:23 +0700

    pustgit

---
+++ src/services/index.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+export const baseHost = 'https://pib-server.onrender.com/'
+export const baseUrl = `${baseHost}/api`
+
+const api = {
+    call : () => {
+        return axios.create({
+            baseURL : baseUrl
+        })
+    }
+}
+
+export default api;
